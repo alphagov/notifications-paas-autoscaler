52bd2056fd3f55ce2a658e72ae0f1c7378b8e996
Athanasios Voutsadakis <athanasios.voutsadakis@digital.cabinet-office.gov.uk>
2018-02-20 14:58:27 +0000

    Allow scheduling API scaling too

--- main.py
+++ main.py
@@ -226,6 +226,11 @@ class AutoScaler:
 
     def scale_elb_app(self, app, paas_app):
         print('Processing {}'.format(app.name))
+        scheduled_desired_instance_count = 0
+        if self.should_scale_on_schedule(app.name):
+            scheduled_desired_instance_count = int(math.ceil(app.max_instance_count * self.scheduled_scale_factor))
+            print("{} to scale to {} on schedule".format(app.name, scheduled_desired_instance_count))
+
         request_counts = self.get_load_balancer_request_counts(app.load_balancer_name)
         if len(request_counts) == 0:
             request_counts = [0]
@@ -235,7 +240,8 @@ class AutoScaler:
         highest_request_count = max(request_counts)
         print('Highest request count (5 min): {}'.format(highest_request_count))
 
-        desired_instance_count = int(math.ceil(highest_request_count / float(app.request_per_instance)))
+        requests_desired_instance_count = int(math.ceil(highest_request_count / float(app.request_per_instance)))
+        desired_instance_count = max(scheduled_desired_instance_count, requests_desired_instance_count)
         self.statsd_client.gauge("{}.request-count".format(app.name), highest_request_count)
         self.scale_paas_apps(app, paas_app, paas_app['instances'], desired_instance_count)
 

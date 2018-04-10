bb85418fe45f80b69c8615ff7114051fbc72c15f
Athanasios Voutsadakis <athanasios.voutsadakis@digital.cabinet-office.gov.uk>
2018-04-10 17:07:07 +0100

    Add one test for run_task and an almost end-to-end one

--- tests/test_autoscaler.py
+++ tests/test_autoscaler.py
@@ -1,6 +1,12 @@
+import datetime
+
 from unittest.mock import patch, Mock
+import yaml
 
 from app.autoscaler import Autoscaler
+from app.elb_scaler import ElbScaler
+from app.schedule_scaler import ScheduleScaler
+from app.app import App
 
 
 @patch('app.autoscaler.Cf')
@@ -71,3 +77,83 @@ class TestAutoscaler:
         autoscaler.scale_paas_apps(app_name, {'guid': app_guid}, 4, 3)
         mock_statsd_client.gauge.assert_called_once_with("{}.instance-count".format(app_name), 4)
         mock_cf.return_value.assert_not_called()
+
+    def test_run_task(self, mock_statsd_client, mock_cf):
+        mock_cf.return_value.get_paas_apps.return_value = {
+            'app1': {'name': 'app1', 'instances': 3, 'guid': 'notify-test-app1'},
+        }
+        mock_autoscaler_app = Mock()
+        mock_autoscaler_app.name = 'app1'
+        mock_autoscaler_app.get_desired_instance_count.return_value = 5
+
+        autoscaler = Autoscaler()
+        autoscaler._schedule = Mock()
+        autoscaler.autoscaler_apps = [mock_autoscaler_app]
+
+        autoscaler.run_task()
+        mock_statsd_client.gauge.assert_called_once_with("{}.instance-count".format('app1'), 5)
+        mock_cf.return_value.apps._update.assert_called_once_with('notify-test-app1', {'instances': 5})
+
+
+@patch('app.autoscaler.Cf')
+@patch.object(Autoscaler, 'statsd_client')
+@patch.object(ScheduleScaler, '_now', return_value=datetime.datetime(2018, 3, 15, 4, 20, 00))
+@patch.object(ElbScaler, 'gauge')
+@patch.object(ElbScaler, '_get_boto3_client')
+@patch.object(ElbScaler, '_get_request_counts')
+class TestAutoscalerAlmostEndToEnd:
+    def test_scale_up(self,
+                      mock_get_request_counts,
+                      mock_get_boto3_client,
+                      mock_gauge,
+                      mock_schedule_scaler_now,
+                      mock_autoscaler_statsd_client,
+                      mock_cf):
+        app_name = 'test-api-app'
+        app_config = {
+            'name': app_name,
+            'scaler_classes': [ElbScaler, ScheduleScaler],
+            'elb_name': 'my-elb',
+            'min_instances': 5,
+            'max_instances': 10,
+            'threshold': 300,
+            'schedule': '''
+---
+workdays:
+  - 08:00-19:00
+weekends:
+  - 09:00-17:00
+scale_factor: 0.8
+'''
+        }
+
+        mock_cf.return_value.get_paas_apps.return_value = {
+            app_name: {'name': app_name, 'instances': 5, 'guid': app_name + '-guid'},
+        }
+
+        # to trigger a scale up we need at least one value greater than min_instances * threshold
+        mock_get_request_counts.return_value = [1300, 1500, 1600, 1700, 1700]
+        app_config['schedule'] = yaml.safe_load(app_config['schedule'])
+        app = App(**app_config)
+
+        autoscaler = Autoscaler()
+        autoscaler.cooldown_seconds_after_scale_up = 300
+        autoscaler.cooldown_seconds_after_scale_down = 60
+        autoscaler._now = Mock(datetime.datetime(2018, 3, 17, 10, 20, 00).timestamp())
+        autoscaler._schedule = Mock()
+
+        autoscaler.autoscaler_apps = [app]
+        autoscaler.run_task()
+
+        mock_autoscaler_statsd_client.gauge.assert_called_once_with("{}.instance-count".format(app_name), 6)
+        mock_cf.return_value.apps._update.assert_called_once_with(app_name + '-guid', {'instances': 6})
+
+        # emulate that we are running in schedule now, which means max_instances * scale_factor
+        mock_schedule_scaler_now.return_value = datetime.datetime(2018, 3, 15, 14, 20, 00)
+        mock_autoscaler_statsd_client.reset_mock()
+        mock_cf.return_value.apps._update.reset_mock()
+
+        autoscaler.run_task()
+
+        mock_autoscaler_statsd_client.gauge.assert_called_once_with("{}.instance-count".format(app_name), 8)
+        mock_cf.return_value.apps._update.assert_called_once_with(app_name + '-guid', {'instances': 8})

781e34bd44378645ac34a2fd0aac20388beb961b
Athanasios Voutsadakis <athanasios.voutsadakis@digital.cabinet-office.gov.uk>
2018-04-10 16:54:52 +0100

    Rename a couple of things for clarity

--- app/app.py
+++ app/app.py
@@ -1,17 +1,17 @@
 class App:
-    def __init__(self, name, scaler_classes, **attributes):
+    def __init__(self, name, scaler_classes, **scaler_details):
         self.name = name
-        self.attributes = attributes
-        self.attributes['app_name'] = name
+        self.scaler_details = scaler_details
+        self.scaler_details['app_name'] = name
         self.scalers = []
         for scaler in scaler_classes:
-            self.scalers.append(scaler(**self.attributes))
+            self.scalers.append(scaler(**self.scaler_details))
 
     def query_scalers(self):
-        instance_counts = []
+        desired_instance_counts = []
         for scaler in self.scalers:
-            instance_counts = scaler.get_desired_instance_count()
-        return instance_counts
+            desired_instance_counts.append(scaler.get_desired_instance_count())
+        return desired_instance_counts
 
     def get_desired_instance_count(self):
         return max(self.query_scalers())

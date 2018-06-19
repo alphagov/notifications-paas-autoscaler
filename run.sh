1bdb8b4cf23cfcfb51afc8cc1310531c9dd4c0fe
Athanasios Voutsadakis <athanasios.voutsadakis@digital.cabinet-office.gov.uk>
2018-06-19 14:36:41 +0100

    Remove the (now unnecessary) run.sh script
    This was used to extract env vars from the $VCAP_SERVICES variable which
    are now being passed directly to the manifest.

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export AWS_SECRET_ACCESS_KEY=`echo $VCAP_SERVICES | jq '.["user-provided"]|map(select(.name == "notify-paas-autoscaler"))|.[0].credentials.aws_secret_access_key' -r`
-export AWS_ACCESS_KEY_ID=`echo $VCAP_SERVICES | jq '.["user-provided"]|map(select(.name == "notify-paas-autoscaler"))|.[0].credentials.aws_access_key_id' -r`
-export CF_USERNAME=`echo $VCAP_SERVICES | jq '.["user-provided"]|map(select(.name == "notify-paas-autoscaler"))|.[0].credentials.cf_username' -r`
-export CF_PASSWORD=`echo $VCAP_SERVICES | jq '.["user-provided"]|map(select(.name == "notify-paas-autoscaler"))|.[0].credentials.cf_password' -r`
-
-exec "$@"

ff9597ff15f93d8b9a07be096debacab9b03faab
David McDonald <david.mcdonald@digital.cabinet-office.gov.uk>
2021-01-13 12:29:53 +0000

    Add metrics for tasks pulled from queue
    We previously had these on our grafana dashboard and they were useful to
    look at during incidents/debugging. They were replaced by metrics for
    how many tasks are being put onto the queue in
    https://github.com/alphagov/notifications-paas-autoscaler/pull/101
    
    Now, however we want these back on our grafana dashboard. This gives us
    a few options of how to do this
    - set up grafana with an AWS Cloudwatch integration
    - create a new app to handle grabbing metrics from AWS Cloudwatch to be
      scraped by prometheus
    - wack the metrics in this app because it has very similar code for the
      number of metrics in the queue and being put onto the queue
    
    Decided to go for the third option due to speed and ease. In the long
    run we may want to go down one of the other routes.
    
    Will require a separate PR to add the statsd mapping for this new
    metric.
    
    I've mostly kept the code as similar as possible to calculating the
    throughput of tasks put onto the queues. This does give us a tiny bit of
    redundant code (we don't really care about the past 5 minutes of data
    for example), but I think the duplicated code makes it easier to
    understand/maintain/debug than having more snowflaked code in here just
    for this quick fix of providing metrics.

--- tests/test_sqs_scaler.py
+++ tests/test_sqs_scaler.py
@@ -97,6 +97,9 @@ class TestSqsScaler:
             sqs_scaler, "_get_desired_instance_count_based_on_throughput_of_tasks_put_onto_queues"
         )
         queue_length_mock = mocker.patch.object(sqs_scaler, "_get_desired_instance_count_based_on_current_queue_length")
+        throughput_tasks_pulled_metric_mock = mocker.patch.object(
+            sqs_scaler, "_publish_metrics_for_throughput_of_tasks_pulled_from_queues"
+        )
         throughput_mock.return_value = 2
         queue_length_mock.return_value = 3
 
@@ -104,6 +107,7 @@ class TestSqsScaler:
 
         throughput_mock.assert_called_once()
         queue_length_mock.assert_called_once()
+        throughput_tasks_pulled_metric_mock.assert_called_once()
 
     def test_get_desired_instance_count_based_on_queue_throughput_of_tasks_put_onto_queue(self, mock_boto3, mocker):
         self.input_attrs['queues'] = ['queue1', 'queue2']
@@ -167,6 +171,64 @@ class TestSqsScaler:
         cloudwatch_client.get_metric_statistics.assert_called_once_with(
             Namespace='AWS/SQS',
             MetricName='NumberOfMessagesSent',
+            Dimensions=[
+                {
+                    'Name': 'QueueName',
+                    'Value': 'my-queue'
+                },
+            ],
+            StartTime=datetime(2018, 3, 15, 15, 5, 0),
+            EndTime=datetime(2018, 3, 15, 15, 10, 0),
+            Period=60,
+            Statistics=['Sum'],
+            Unit='Count'
+        )
+
+    def test_get_throughput_of_tasks_pulled_from_queue_uses_max_value(self, mock_boto3, mocker):
+        sqs_scaler = SqsScaler(app_name, min_instances, max_instances, **self.input_attrs)
+
+        _get_sqs_throughput_mock = mocker.patch.object(
+            sqs_scaler, '_get_sqs_throughput_of_tasks_pulled_from_queue', return_value=[100, 200, 50]
+        )
+        statsd_mock = mocker.patch.object(sqs_scaler, 'statsd_client')
+
+        assert sqs_scaler._get_throughput_of_tasks_pulled_from_queue('my-queue') == 200
+        # queue prefix "test" pulled from SQS_QUEUE_PREFIX env var
+        statsd_mock.gauge.assert_called_once_with('testmy-queue.throughput-tasks-pulled-from-queue', 50)
+        _get_sqs_throughput_mock.assert_called_once_with('testmy-queue')
+
+    def test_get_throughput_of_tasks_pulled_from_queue_returns_0_if_no_data(self, mock_boto3, mocker):
+        sqs_scaler = SqsScaler(app_name, min_instances, max_instances, **self.input_attrs)
+
+        mocker.patch.object(
+            sqs_scaler, '_get_sqs_throughput_of_tasks_pulled_from_queue', return_value=[]
+        )
+        statsd_mock = mocker.patch.object(sqs_scaler, 'statsd_client')
+
+        assert sqs_scaler._get_throughput_of_tasks_pulled_from_queue('my-queue') == 0
+        statsd_mock.gauge.assert_called_once_with('testmy-queue.throughput-tasks-pulled-from-queue', 0)
+
+    @freeze_time("2018-03-15 15:10:00")
+    def test_get_sqs_throughput_of_tasks_pulled_from_queue(self, mock_boto3):
+        self.input_attrs['queues'] = ['queue1', 'queue2']
+
+        cloudwatch_client = mock_boto3.client.return_value
+        cloudwatch_client.get_metric_statistics.return_value = {
+            'Datapoints': [
+                {'Sum': 1500, 'Timestamp': 111111110},
+                {'Sum': 1600, 'Timestamp': 111111111},
+                {'Sum': 5500, 'Timestamp': 111111112},
+                {'Sum': 5300, 'Timestamp': 111111113},
+                {'Sum': 2100, 'Timestamp': 111111114},
+            ]}
+
+        sqs_scaler = SqsScaler(app_name, min_instances, max_instances, **self.input_attrs)
+
+        assert sqs_scaler._get_sqs_throughput_of_tasks_pulled_from_queue('my-queue') == [1500, 1600, 5500, 5300, 2100]
+
+        cloudwatch_client.get_metric_statistics.assert_called_once_with(
+            Namespace='AWS/SQS',
+            MetricName='NumberOfMessagesReceived',
             Dimensions=[
                 {
                     'Name': 'QueueName',
